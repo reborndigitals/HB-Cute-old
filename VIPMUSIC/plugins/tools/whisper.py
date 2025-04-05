39d6f9cf7d0933edd0995ae0e38381b19a2e3220
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2025-04-05 12:16:13 +0530

    whisper.py

--- VIPMUSIC/plugins/tools/whisper.py
+++ VIPMUSIC/plugins/tools/whisper.py
@@ -103,7 +103,7 @@ async def in_help():
         InlineQueryResultArticle(
             title="⦿ Whisper ⦿",
             description=f"@{BOT_USERNAME} [USERNAME | ID] [TEXT]",
-            input_message_content=InputTextMessageContent(f"**❍ Usage:**\n\n@{BOT_USERNAME} (Target Username or ID) (Your Message).\n\n**Example:**\n@{BOT_USERNAME} @username I Wanna Phuck You"),
+            input_message_content=InputTextMessageContent(f"**❍ Usage:**\n\n@{BOT_USERNAME} (Target Username or ID) (Your Message).\n\n**Example:**\n@{BOT_USERNAME} @username Join @HeartBeat_Muzic Chat Group"),
             thumb_url="https://graph.org/file/ffdb1be822436121cf5fd.png",
             reply_markup=switch_btn
         )
