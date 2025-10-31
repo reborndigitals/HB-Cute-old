19bb69e160084410613c72805cc2918b83f716d9
reborndigitals <reborndigitals@gmail.com>
2025-10-31 23:39:27 +0530

    play.py

--- VIPMUSIC/utils/inline/play.py
+++ VIPMUSIC/utils/inline/play.py
@@ -267,6 +267,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
 
 def stream_markup(_, videoid, chat_id):
     buttons = [
+    [
         InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
         InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
         InlineKeyboardButton(text="↺", callback_data=f"ADMIN Replay|{chat_id}"),
