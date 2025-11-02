3884f471379f51b4c78ac7b8e1ba9aa15c612357
reborndigitals <reborndigitals@gmail.com>
2025-11-02 16:30:05 +0530

    language.py

--- VIPMUSIC/utils/decorators/language.py
+++ VIPMUSIC/utils/decorators/language.py
@@ -1,49 +1,45 @@
 from VIPMUSIC.misc import SUDOERS
 from VIPMUSIC.utils.database import get_lang, is_maintenance
 from strings import get_string
-from config import SUPPORT_CHAT
-from VIPMUSIC import app
 
 
 def language(mystic):
     async def wrapper(_, message, **kwargs):
-        # Maintenance check
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ.\nVisit <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴅᴇᴛᴀɪʟs.",
+                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
                     disable_web_page_preview=True,
                 )
         try:
             await message.delete()
-        except Exception:
+        except:
             pass
 
         try:
-            lang_code = await get_lang(message.chat.id)
-            _ = get_string(lang_code)
-        except Exception:
-            _ = get_string("en")
-        return await mystic(_, message, _)
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
 
     return wrapper
 
 
 def languageCB(mystic):
     async def wrapper(_, CallbackQuery, **kwargs):
-        # Maintenance check
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ.\nVisit sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴅᴇᴛᴀɪʟs.",
+                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
                     show_alert=True,
                 )
         try:
-            lang_code = await get_lang(CallbackQuery.message.chat.id)
-            _ = get_string(lang_code)
-        except Exception:
-            _ = get_string("en")
-        return await mystic(_, CallbackQuery, _)
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
 
     return wrapper
 
@@ -51,10 +47,10 @@ def languageCB(mystic):
 def LanguageStart(mystic):
     async def wrapper(_, message, **kwargs):
         try:
-            lang_code = await get_lang(message.chat.id)
-            _ = get_string(lang_code)
-        except Exception:
-            _ = get_string("en")
-        return await mystic(_, message, _)
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
 
     return wrapper
