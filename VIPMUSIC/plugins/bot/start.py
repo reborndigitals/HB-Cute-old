26773e428e694c8f468093c2de8a455e8304687c
reborndigitals <reborndigitals@gmail.com>
2025-11-03 16:39:58 +0530

    start.py

--- VIPMUSIC/plugins/bot/start.py
+++ VIPMUSIC/plugins/bot/start.py
@@ -131,7 +131,7 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
-    out = private_panel(_)
+        out = private_panel(_)
 
     # Step 1: Greet with random emoji
     loading_1 = await message.reply_text(random.choice(GREET))
